3c0c5ad6d5c3916db8c046a3e9873917ceaf55f1
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-07-16 17:40:31 +0530

    fetch

---
+++ src/java/controller/VerifyAccount.java
@@ -0,0 +1,25 @@
+package controller;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Laky
+ */
+@WebServlet(name = "VerifyAccount", urlPatterns = {"/VerifyAccount"})
+public class VerifyAccount extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        System.out.println("done");
+    }
+}
---
+++ web/en/assets/js/verify-email.js
@@ -0,0 +1,23 @@
+async function VerifyAccount() {
+
+    const inputs = document.querySelectorAll('.verification-code-input input');
+    let verificationCode = '';
+
+    inputs.forEach(input => {
+        verificationCode += input.value.trim();
+    });
+
+    const verification = {
+        verificationCode: verificationCode
+    };
+
+    const response = await fetch("../../VerifyAccount", {
+        method: "POST",
+        body: JSON.stringify(verification),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+
+}
