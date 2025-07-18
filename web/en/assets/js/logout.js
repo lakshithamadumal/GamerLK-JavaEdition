ca578d74cd46c59e98e29cc213509fc320464b8a
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-07-18 18:09:31 +0530

    logout

--- web/en/assets/js/logout.js
+++ web/en/assets/js/logout.js
@@ -1,10 +1,25 @@
 async function LogOut() {
+    const response = await fetch("../../SignOut");
+
+    if (response.ok) {
+        const json = await response.json();
+        if (json.status) {
+            window.location.reload();
+        } else {
+            window.location.reload();
+        }
+    } else {
+        console.log(response);
+    }
+}
+
+async function LogOutIndex() {
     const response = await fetch("../SignOut");
 
     if (response.ok) {
         const json = await response.json();
         if (json.status) {
-            window.location = "/Gamerlk/en/pages/login.html"; 
+            window.location.reload();
         } else {
             window.location.reload();
         }
