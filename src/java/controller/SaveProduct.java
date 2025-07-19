f6d9309288fdef75f7a32b596fcafbd835b82d15
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-07-19 07:01:44 +0530

    product load

---
+++ src/java/controller/SaveProduct.java
@@ -0,0 +1,57 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author Laky
+ */
+@MultipartConfig
+@WebServlet(name = "SaveProduct", urlPatterns = {"/SaveProduct"})
+public class SaveProduct extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String gameName = request.getParameter("gameName");
+        String gameDescription = request.getParameter("gameDescription");
+        String gamePrice = request.getParameter("gamePrice");
+        String gameLink = request.getParameter("gameLink");
+        String gameSize = request.getParameter("gameSize");
+        String gameDate = request.getParameter("gameDate");
+        String gameCategory = request.getParameter("gameCategory");
+        String gameMod = request.getParameter("gameMod");
+        String gameDeveloper = request.getParameter("gameDeveloper");
+        String gameTag = request.getParameter("gameTag");
+        String gameMin = request.getParameter("gameMin");
+        String gameMax = request.getParameter("gameMax");
+        Part cardImage = request.getPart("cardImage");
+        Part thumbnailImage = request.getPart("thumbnailImage");
+
+        System.out.println("Game Name: " + gameName);
+        System.out.println("Game Description: " + gameDescription);
+        System.out.println("Game Price: " + gamePrice);
+        System.out.println("Game Link: " + gameLink);
+        System.out.println("Game Size: " + gameSize);
+        System.out.println("Game Date: " + gameDate);
+        System.out.println("Game Category: " + gameCategory);
+        System.out.println("Game Mode: " + gameMod);
+        System.out.println("Game Developer: " + gameDeveloper);
+        System.out.println("Game Tag: " + gameTag);
+        System.out.println("Minimum Requirement: " + gameMin);
+        System.out.println("Maximum Requirement: " + gameMax);
+        System.out.println("Card Image: " + cardImage.getSubmittedFileName());
+        System.out.println("Thumbnail Image: " + thumbnailImage.getSubmittedFileName());
+
+    }
+
+}
--- web/admin/assets/js/loadProduct.js
+++ web/admin/assets/js/loadProduct.js
@@ -34,3 +34,47 @@ function loadSelect(selectId, list, property) {
     });
 
 }
+
+
+async function addGame() {
+
+    const gameName = document.getElementById("gameName").value;
+    const gameDescription = document.getElementById("gameDescription").value;
+    const gamePrice = document.getElementById("gamePrice").value;
+    const gameLink = document.getElementById("gameLink").value;
+    const gameSize = document.getElementById("gameSize").value;
+    const gameDate = document.getElementById("gameDate").value;
+    const gameCategory = document.getElementById("gameCategory").value;
+    const gameMod = document.getElementById("gameMod").value;
+    const gameDeveloper = document.getElementById("gameDeveloper").value;
+    const gameTag = document.getElementById("gameTag").value;
+    const gameMin = document.getElementById("gameMin").value;
+    const gameMax = document.getElementById("gameMax").value;
+    const cardImage = document.getElementById("cardImage").files[0];
+    const thumbnailImage = document.getElementById("thumbnailImage").files[0];
+
+
+    const form = new FormData();
+    form.append("gameName", gameName);
+    form.append("gameDescription", gameDescription);
+    form.append("gamePrice", gamePrice);
+    form.append("gameLink", gameLink);
+    form.append("gameSize", gameSize);
+    form.append("gameDate", gameDate);
+    form.append("gameCategory", gameCategory);
+    form.append("gameMod", gameMod);
+    form.append("gameDeveloper", gameDeveloper);
+    form.append("gameTag", gameTag);
+    form.append("gameMin", gameMin);
+    form.append("gameMax", gameMax);
+    form.append("cardImage", cardImage);
+    form.append("thumbnailImage", thumbnailImage);
+
+    const response = await fetch("../SaveProduct", {
+        method: "POST",
+        body: form,
+    });
+
+    const result = await response.text();
+    console.log("Server Response:", result);
+}
