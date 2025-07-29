de299f9cca0396c87a1034696923c473b28e10f8
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-07-29 11:44:05 +0530

    load [roduct

--- web/en/assets/js/loadStoreGames.js
+++ web/en/assets/js/loadStoreGames.js
@@ -14,20 +14,32 @@ document.addEventListener("DOMContentLoaded", async function () {
         let storeGames = document.getElementById("game-card");
 
         storeGamesContainer.innerHTML = "";
-        json.productList.forEach(gamelist => {
-            let productCloneHtml = storeGames.cloneNode(true);
 
-            // Set data
-            productCloneHtml.querySelector("a").href = "game-details.html?id=" + gamelist.id;
-            productCloneHtml.querySelector("img").src = "../../assets/Games\\" + gamelist.id + "\\thumb-image.jpg";
-            productCloneHtml.querySelector(".game-title").innerHTML = gamelist.title;
-            productCloneHtml.querySelector(".game-price-row span").innerHTML = "$" + new Intl.NumberFormat(
-                "en-US",
-                { minimumFractionDigits: 2 }
-            ).format(gamelist.price);
+        // Group by 4
+        for (let i = 0; i < json.productList.length; i += 4) {
+            // Create new game-list div for each group of 4
+            let groupDiv = document.createElement("div");
+            groupDiv.className = "game-list";
 
-            storeGamesContainer.appendChild(productCloneHtml);
-        });
+            // For each product in this group
+            for (let j = i; j < i + 4 && j < json.productList.length; j++) {
+                let gamelist = json.productList[j];
+                let productCloneHtml = storeGames.cloneNode(true);
+
+                // Set data
+                productCloneHtml.querySelector("a").href = "game-details.html?id=" + gamelist.id;
+                productCloneHtml.querySelector("img").src = "../../assets/Games\\" + gamelist.id + "\\thumb-image.jpg";
+                productCloneHtml.querySelector(".game-title").innerHTML = gamelist.title;
+                productCloneHtml.querySelector(".game-price-row span").innerHTML = "$" + new Intl.NumberFormat(
+                    "en-US",
+                    { minimumFractionDigits: 2 }
+                ).format(gamelist.price);
+
+                groupDiv.appendChild(productCloneHtml);
+            }
+
+            storeGamesContainer.appendChild(groupDiv);
+        }
 
     } else {
         notyf.error({
