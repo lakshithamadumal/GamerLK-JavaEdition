eb4bb4e6cae9fdb94727e97a4db89e85179de3d8
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-08-11 13:36:38 +0530

    Update SendPurchaseEmailServlet.java

--- src/java/controller/SendPurchaseEmailServlet.java
+++ src/java/controller/SendPurchaseEmailServlet.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class SendPurchaseEmailServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String orderIdStr = request.getParameter("orderId");
